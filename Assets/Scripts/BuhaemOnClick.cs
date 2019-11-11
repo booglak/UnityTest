42d92a6738796cc20480ffbea13184003b69d8cc
Igor Buglak <i.buglak@nullgr.com>
2019-11-11 16:01:05 +0200

    Alco new initial

---
+++ Assets/Scripts/BuhaemOnClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuhaemOnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btnBuhaem = GetComponent<Button>();
+        btnBuhaem.onClick.AddListener(buhaemOnClick);
+        
+    }
+    private Sprite sprite1, sprite2, sprite3;
+    private GameObject image;
+    public void buhaemOnClick()
+    {
+        image = GameObject.Find("MainImage");
+        sprite2 = Resources.Load<Sprite>("Images/second_image");
+        image.GetComponent<Image>().sprite = sprite2;
+    }
+    
+
+
+}
---
+++ Assets/Scripts/StarOnClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class StarOnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btnStart = GetComponent<Button>();
+        btnStart.onClick.AddListener(startOnClick);
+    }
+
+    void startOnClick(){
+        SceneManager.LoadScene("Scenes/Scene2");
+    }
+
+
+}
